b64e53888ba96569e435f28fcb74abadce7ffe4f
smiz <simachewbekalubayih1616@gmail.com>
2022-12-29 10:18:35 +0000

    this is done by simachew

---
+++ smiz.py
@@ -0,0 +1,27 @@
+
+x,y,z="Orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+a=4
+b=8
+c="number"
+print(len(c))
+d=input("Enter your name: ")
+print(d)
+print(type(d))
+e=input("Enter first number: ")
+f=input("Enter the second number: ")
+g=int(e)+int(f)
+print(g)
+print(ord('g'))
+name="simachew"
+age="30"
+print("my name is {} and i am years old {} " . format(name,age))
+def sum():
+    print("Hello world!")
+sum()
